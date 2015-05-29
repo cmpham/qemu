09400ecf08f7ef53775a559cd6d035814412823b
Cuong Pham <phammanhcuong@gmail.com>
2015-05-29 12:40:55 -0500

    SHA for hsafe.

---
+++ hsafe/hs.h
@@ -0,0 +1,13 @@
+/**
+ * hs.h
+ * Cryptographic signature for execution traces
+ * Author: Cuong Pham (phammanhcuong@gmail.com)
+ * May 28, 2015
+ **/
+#pragma once
+
+static inline void gen_instr_end(DisasContext *s);
+
+static inline void gen_block_start(DisasContext *s, struct TranslationBlock *tb, uint64_t pc);
+
+static inline void gen_block_end(DisasContext *s /*, struct TranslationBlock *tb*/);
